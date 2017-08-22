b76b90390db551f738f659bf084e75d1b48b128e
Lucas Alves Sobrinho <lucasalves_22@hotmail.com>
2017-08-22 20:04:54 -0300

    Implementing Naive Bayes on wine dataset, refactoring code

--- monolithic_wine.py
+++ monolithic_wine.py
@@ -2,12 +2,31 @@
 import numpy as np
 from sklearn.model_selection import cross_val_score
 from sklearn import tree
+from sklearn.naive_bayes import GaussianNB
+
+
+print("\nMonolithic Methods\n")
+
+# -----------------------------------------------------------------------------
+# Global Parameters
+
+cvFolds = 10
+datasetFolderName = 'UCI_Datasets/'
+datasetFileName = 'wine.data'
+
+
+# -----------------------------------------------------------------------------
+# Global Methods
+
+def printCVAccuracy(scores):
+	print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+	print
+
 
 # -----------------------------------------------------------------------------
 # Data Preparation
 
-wine_fileName = 'UCI_Datasets/wine.data'
-wine_dataset = np.loadtxt(wine_fileName, delimiter=",")
+wine_dataset = np.loadtxt(datasetFolderName + datasetFileName, delimiter=",")
 
 wine_data = wine_dataset[:, 1:13]
 wine_target = wine_dataset[:, 0]
@@ -16,6 +35,16 @@ wine_target = wine_dataset[:, 0]
 # -----------------------------------------------------------------------------
 # Decision Tree
 
+print("Decision Tree")
 clf = tree.DecisionTreeClassifier()
-scores = cross_val_score(clf, wine_data, wine_target, cv=10)
-print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores = cross_val_score(clf, wine_data, wine_target, cv=cvFolds)
+printCVAccuracy(scores)
+
+
+# -----------------------------------------------------------------------------
+# Naive Bayes (Gaussian)
+
+print("Naive Bayes (Gaussian)")
+gnb = GaussianNB()
+scores = cross_val_score(gnb, wine_data, wine_target, cv=cvFolds)
+printCVAccuracy(scores)
