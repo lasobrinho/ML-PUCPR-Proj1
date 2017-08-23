9d5788b05a4f04caf36ad0a7bb7ccd9d6832e45c
Lucas Alves Sobrinho <lucasalves_22@hotmail.com>
2017-08-23 00:04:05 -0300

    Making folds to shuffle samples when performing cross-validation

--- monolithic_wine.py
+++ monolithic_wine.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
 from sklearn import tree
 from sklearn.naive_bayes import GaussianNB
 from sklearn import neighbors
@@ -8,7 +9,7 @@ from sklearn.neural_network import MLPClassifier
 from sklearn import svm
 
 
-print("\nMonolithic Methods\n")
+print("\nMonolithic Classification - Wine dataset\n")
 
 # -----------------------------------------------------------------------------
 # Global Parameters
@@ -16,21 +17,20 @@ print("\nMonolithic Methods\n")
 cvFolds = 10
 datasetFolderName = 'UCI_Datasets/'
 datasetFileName = 'wine.data'
+shuffle = KFold(n_splits=cvFolds, shuffle=True, random_state=1)
 
 
 # -----------------------------------------------------------------------------
 # Global Methods
 
 def printCVAccuracy(scores):
-	print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-	print
+	print("  Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
 
 # -----------------------------------------------------------------------------
 # Data Preparation
 
 wine_dataset = np.loadtxt(datasetFolderName + datasetFileName, delimiter=",")
-
 wine_data = wine_dataset[:, 1:13]
 wine_target = wine_dataset[:, 0]
 
@@ -40,7 +40,7 @@ wine_target = wine_dataset[:, 0]
 
 print("Decision Tree")
 dtc = tree.DecisionTreeClassifier()
-scores = cross_val_score(dtc, wine_data, wine_target, cv=cvFolds)
+scores = cross_val_score(dtc, wine_data, wine_target, cv=shuffle)
 printCVAccuracy(scores)
 
 
@@ -49,7 +49,7 @@ printCVAccuracy(scores)
 
 print("Naive Bayes (Gaussian)")
 gnb = GaussianNB()
-scores = cross_val_score(gnb, wine_data, wine_target, cv=cvFolds)
+scores = cross_val_score(gnb, wine_data, wine_target, cv=shuffle)
 printCVAccuracy(scores)
 
 
@@ -60,12 +60,12 @@ k = 10
 
 print("K-Nearest Neighbors (K=10, Uniform Weights)")
 knn = neighbors.KNeighborsClassifier(k, weights='uniform')
-scores = cross_val_score(gnb, wine_data, wine_target, cv=cvFolds)
+scores = cross_val_score(knn, wine_data, wine_target, cv=shuffle)
 printCVAccuracy(scores)
 
 print("K-Nearest Neighbors (K=10, Distance Weights)")
 knn = neighbors.KNeighborsClassifier(k, weights='distance')
-scores = cross_val_score(knn, wine_data, wine_target, cv=cvFolds)
+scores = cross_val_score(knn, wine_data, wine_target, cv=shuffle)
 printCVAccuracy(scores)
 
 
@@ -74,7 +74,7 @@ printCVAccuracy(scores)
 
 print("Multi-Layer Perceptron (MLP)")
 mlp = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5), random_state=1)
-scores = cross_val_score(mlp, wine_data, wine_target, cv=cvFolds)
+scores = cross_val_score(mlp, wine_data, wine_target, cv=shuffle)
 printCVAccuracy(scores)
 
 
@@ -83,10 +83,10 @@ printCVAccuracy(scores)
 
 print("Support Vector Machine (SVM - Linear)")
 linearSVM = svm.LinearSVC()
-scores = cross_val_score(linearSVM, wine_data, wine_target, cv=cvFolds)
+scores = cross_val_score(linearSVM, wine_data, wine_target, cv=shuffle)
 printCVAccuracy(scores)
 
-print("Support Vector Machine (SVM - SVC/RBF)")
+print("Support Vector Machine (SVM - RBF)")
 rbfSVM = svm.SVC()
-scores = cross_val_score(rbfSVM, wine_data, wine_target, cv=cvFolds)
+scores = cross_val_score(rbfSVM, wine_data, wine_target, cv=shuffle)
 printCVAccuracy(scores)
